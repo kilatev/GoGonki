d2b9feee6dad6381d4c851c39edb5d698faabae2
vetalik <vetalik@gmail.com>
2014-04-30 06:11:14 +0000

    playing with revel. no actual code yet

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -7,5 +7,19 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+  greeting := "Aloha World"
+	return c.Render(greeting)
+}
+
+func (c App) Hello(myName string) revel.Result {
+  c.Validation.Required(myName).Message("Your Name is required!")
+  c.Validation.MinSize(myName, 3).Message("Your name should be at least 3 symbols")
+  
+  if c.Validation.HasErrors() {
+    c.Validation.Keep()
+    c.FlashParams()
+    return c.Redirect(App.Index)
+  }
+  
+  return c.Render(myName)
 }
