2467d3537dc54e09a0ae7e0b9cc2c9adf8c56c08
vetalik <vetalik@gmail.com>
2014-05-10 13:32:44 +0000

    added tests and wercker config has more stuff

---
+++ main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "encoding/json"
+    "log"
+    "bytes"
+
+  . "github.com/onsi/ginkgo"
+  . "github.com/onsi/gomega"
+)
+var _ = Describe("Todo", func() {
+
+    var (
+        body []byte
+        err error
+        //todos []Todo
+    )
+
+    Context("Get coords", func() {
+        It("returns a 200 Status Code", func() {
+            Request("GET", "/coords", Coords)
+            Expect(response.Code).To(Equal(200))
+        })
+    })
+
+    // Context("Create a Todo", func() {
+
+    //     BeforeEach(func() {
+    //         todo := Todo{"keep things green"}
+    //         body, err = json.Marshal(todo)
+    //         if err != nil {
+    //             log.Println("Unable to marshal todo")
+    //         }
+    //     })
+
+    //     It("returns a 200 Status Code", func() {
+    //         PostRequest("POST", "/todos", HandleNewTodo, bytes.NewReader(body))
+    //         Expect(response.Code).To(Equal(200))
+    //     })
+    // })
+
+    // Context("Returns a single created todo", func() {
+    //     It("returns a 200 Status Code", func() {
+    //         Request("GET", "/todos", HandleIndex)
+    //         log.Println(response)
+    //         log.Println(response.Body)
+    //         Expect(response.Code).To(Equal(200))
+    //         Expect(response.Body).To(MatchJSON(`[{"Name":"keep things green"}]`))
+    //     })
+    // })
+})
---
+++ mainsuitetest.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  . "github.com/onsi/ginkgo"
+  . "github.com/onsi/gomega"
+  "testing"
+    "io"
+  "net/http"
+  "net/http/httptest"
+    "github.com/codegangsta/martini"
+    "github.com/codegangsta/martini-contrib/render"
+    "github.com/codegangsta/martini-contrib/binding"
+
+)
+
+var (
+  response *httptest.ResponseRecorder
+)
+
+func TestMain(t *testing.T) {
+  RegisterFailHandler(Fail)
+  RunSpecs(t, "Main Suite")
+}
+
+func Request(method string, route string, handler martini.Handler) {
+  m := martini.Classic()
+  m.Get(route, handler)
+  m.Use(render.Renderer())
+  request, _ := http.NewRequest(method, route, nil)
+  response = httptest.NewRecorder()
+  m.ServeHTTP(response, request)
+}
+
+func PostRequest(method string, route string, handler martini.Handler, body io.Reader) {
+  m := martini.Classic()
+  m.Post(route, binding.Json(Todo{}), handler)
+  m.Use(render.Renderer())
+  request, _ := http.NewRequest(method, route, body)
+  response = httptest.NewRecorder()
+  m.ServeHTTP(response, request)
+}
