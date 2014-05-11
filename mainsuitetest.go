ec1f3951fb042299de32c1e79cce284ee2ceeb45
vetalik <vetalik@gmail.com>
2014-05-11 16:31:26 +0300

    adding new routes
    just added new route. need to learn more about go martini and go json

--- mainsuitetest.go
+++ mainsuitetest.go
@@ -1,41 +1,40 @@
 package main
 
 import (
-  . "github.com/onsi/ginkgo"
-  . "github.com/onsi/gomega"
-  "testing"
-    //"io"
-  "net/http"
-  "net/http/httptest"
-    "github.com/codegangsta/martini"
-    "github.com/codegangsta/martini-contrib/render"
-    //"github.com/codegangsta/martini-contrib/binding"
-
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+	//"io"
+	"github.com/codegangsta/martini"
+	"github.com/codegangsta/martini-contrib/render"
+	"net/http"
+	"net/http/httptest"
+	//"github.com/codegangsta/martini-contrib/binding"
 )
 
 var (
-  response *httptest.ResponseRecorder
+	response *httptest.ResponseRecorder
 )
 
 func TestMain(t *testing.T) {
-  RegisterFailHandler(Fail)
-  RunSpecs(t, "Main Suite")
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Main Suite")
 }
 
 func Request(method string, route string, handler martini.Handler) {
-  m := martini.Classic()
-  m.Get(route, handler)
-  m.Use(render.Renderer())
-  request, _ := http.NewRequest(method, route, nil)
-  response = httptest.NewRecorder()
-  m.ServeHTTP(response, request)
+	m := martini.Classic()
+	m.Get(route, handler)
+	m.Use(render.Renderer())
+	request, _ := http.NewRequest(method, route, nil)
+	response = httptest.NewRecorder()
+	m.ServeHTTP(response, request)
 }
 
-// func PostRequest(method string, route string, handler martini.Handler, body io.Reader) {
-//   m := martini.Classic()
-//   m.Post(route, binding.Json(Todo{}), handler)
-//   m.Use(render.Renderer())
-//   request, _ := http.NewRequest(method, route, body)
-//   response = httptest.NewRecorder()
-//   m.ServeHTTP(response, request)
-// }
+/*func PostRequest(method string, route string, handler martini.Handler, body io.Reader) {
+	m := martini.Classic()
+	m.Post(route, binding.Json(Todo{}), handler)
+	m.Use(render.Renderer())
+	request, _ := http.NewRequest(method, route, body)
+	response = httptest.NewRecorder()
+	m.ServeHTTP(response, request)
+}*/
--- server.go
+++ server.go
@@ -1,38 +1,43 @@
 package main
 
 import (
-    "github.com/go-martini/martini"
+	"fmt"
+	"github.com/go-martini/martini"
 )
 
-
 type Point struct {
-  X int
-  Y int
+	X int
+	Y int
 }
 
 type Car struct {
-  Color int
-  Point Point
+	Color int
+	Point Point
 }
 
 func main() {
-  m := martini.Classic()
-  m.Get("/", Index)
-  
-  m.Get("/coords", Coords)
-  m.Get("/move/:x/:y", Move) // ToDo: make this post reqeust
-  m.Run()
+	m := martini.Classic()
+	m.Get("/", Index)
+
+	m.Get("/coords", Coords)
+	m.Get("/move/:x/:y", Move) // ToDo: make this post reqeust
+	m.Get("/state", State)
+	m.Run()
 }
 
-
 func Index() string {
-    return "This is index"
+	return "This is index"
 }
 
 func Coords() string {
-    return "coords"
+	return "coords"
 }
 
-func Move(params martini.Params) (string) { // this is fake function for now. ToDo: change it to use actual Point
-    return "Hello " + params["x"] + params["y"]
+func Move(params martini.Params) string { // this is fake function for now. ToDo: change it to use actual Point
+	fmt.Println("Scuka kruta")
+	return "Hello " + params["x"] + params["y"]
+}
+
+func State(params martini.Params) string { // Todo: change return type to json
+	return "this is my state"
 }
