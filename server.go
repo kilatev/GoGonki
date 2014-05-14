2882fb9db7499fc9be785d0d5b30201df80af063
vetalik <vetalik@gmail.com>
2014-05-14 21:47:58 +0300

    adding gorp persistance

--- server.go
+++ server.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-//	"database/sql"
+	//	"database/sql"
 	"encoding/json"
-//	"fmt"
-//	"github.com/codegangsta/martini-contrib/binding"
-	"github.com/coopernurse/gorp"
+	//	"fmt"
+	"github.com/codegangsta/martini-contrib/binding"
+	//	"github.com/coopernurse/gorp"
 	"github.com/go-martini/martini"
-//	_ "github.com/mattn/go-sqlite3"
+	//	_ "github.com/mattn/go-sqlite3"
 	"log"
 )
 
@@ -78,7 +78,7 @@ func main() {
 }
 
 func Index() string {
-    log.Println("This is index page")
+	log.Println("This is index page")
 	return "This is index"
 }
 
@@ -116,31 +116,7 @@ func Logout(params martini.Params) (result string) {
 	return
 }
 
-
-func initDb() *gorp.DbMap {
-	// connect to db using standard Go database/sql API
-	// use whatever database/sql driver you wish
-	db, err := sql.Open("sqlite3", "./tmp/post_db.bin")
-	checkErr(err, "sql.Open failed")
-
-	// construct a gorp DbMap
-	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
-
-	// add a table, setting the table name to 'posts' and
-	// specifying that the Id property is an auto incrementing PK
-	dbmap.AddTableWithName(User{}, "posts").SetKeys(true, "Id")
-
-	// create the table. in a production system you'd generally
-	// use a migration tool, or create the tables via scripts
-	err = dbmap.CreateTablesIfNotExists()
-	checkErr(err, "Create tables failed")
-
-	return dbmap
-}
-
-
-func checkErr(err error, msg string) {
-    if err != nil {
-        log.Fatalln(msg, err)
-    }
+func CreateUser(user User) (result string) {
+	user.Save()
+	return
 }
