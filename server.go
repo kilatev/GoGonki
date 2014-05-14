cf3e6df5c59d7ef47a9ff8862b45d6b9e290882a
vetalik <vetalik@gmail.com>
2014-05-14 07:47:44 +0000

    added unittests. db support is growing. branch should not fail

--- server.go
+++ server.go
@@ -3,7 +3,7 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
+//	"fmt"
 	"github.com/codegangsta/martini-contrib/binding"
 	"github.com/coopernurse/gorp"
 	"github.com/go-martini/martini"
@@ -77,6 +77,7 @@ func main() {
 }
 
 func Index() string {
+    log.Println("This is index page")
 	return "This is index"
 }
 
@@ -117,8 +118,8 @@ func Logout(params martini.Params) (result string) {
 func CreateUser(user User) User {
 	dbmap := initDb()
 	defer dbmap.Db.Close()
-
-	return
+    user = User{}
+	return user
 }
 
 func initDb() *gorp.DbMap {
@@ -132,7 +133,7 @@ func initDb() *gorp.DbMap {
 
 	// add a table, setting the table name to 'posts' and
 	// specifying that the Id property is an auto incrementing PK
-	dbmap.AddTableWithName(Post{}, "posts").SetKeys(true, "Id")
+	dbmap.AddTableWithName(User{}, "posts").SetKeys(true, "Id")
 
 	// create the table. in a production system you'd generally
 	// use a migration tool, or create the tables via scripts
@@ -140,4 +141,11 @@ func initDb() *gorp.DbMap {
 	checkErr(err, "Create tables failed")
 
 	return dbmap
+}
+
+
+func checkErr(err error, msg string) {
+    if err != nil {
+        log.Fatalln(msg, err)
+    }
 }
