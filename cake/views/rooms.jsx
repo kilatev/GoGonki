71dd32bd250d698b23899d9641ba8661356933e7
vetalik <vetalik@gmail.com>
2014-06-01 22:31:30 +0300

    adding rooms page sceleton

---
+++ cake/views/rooms.jsx
@@ -0,0 +1,48 @@
+/** @jsx React.DOM */
+
+var Register = React.createClass({
+  render: function() {
+    return (
+      <table>
+          <th>
+              <td></td>
+          </th>
+          <th>
+              <td></td>
+          </th>
+          <th>
+              <td></td>
+          </th>
+          <th>
+              <td></td>
+          </th>
+          <th>
+              <td></td>
+          </th>
+          <th>
+              <td></td>
+          </th>
+
+          <tr>
+              <td></td>
+          </tr>
+          <tr>
+              <td></td>
+          </tr>
+          <tr>
+              <td></td>
+          </tr>
+          <tr>
+              <td></td>
+          </tr>
+          <tr>
+              <td></td>
+          </tr>
+          <tr>
+              <td></td>
+          </tr>
+      </table>
+
+    );
+  }
+});
